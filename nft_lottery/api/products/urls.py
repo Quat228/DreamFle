9482e149e094f40135f65aa2642ceb99752c4274
Quat228 <zharkulov04@mail.ru>
2025-11-23 23:42:55 -0800

    added user auth by Telegram Mini App

--- nft_lottery/api/products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import ProductPurchaseAPIView
-
-urlpatterns = [
-    path("purchase/", ProductPurchaseAPIView.as_view(), name="product-purchase"),
-]
-
-
--- nft_lottery/api/urls.py
+++ nft_lottery/api/urls.py
@@ -2,5 +2,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path("users/", include("api.users.urls")),
-    path("products/", include("api.products.urls")),
 ]
